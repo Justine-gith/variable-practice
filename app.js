592c7bcc46012bf847027e6d08d7c9a0450d39ee
unknown <ganijustine8@gmail.com>
2024-09-08 11:15:17 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//javascript_VARIABLES
+let age = 10; 
+console.log(age);
+
+let score = 95;
+console.log(score);
+
+//if want to be permanent use const.keyword
+const pi = 3.14259;
+console.log(pi);
+
+let isStudent = true;
+console.log(isStudent);
+
+let colors = ['red', 'green', 'yellow'];
+console.log(colors);
